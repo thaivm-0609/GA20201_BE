016d7e89999d29cd260adae2f18345b0efebd32a
thaivm-0609 <vuongthai95@gmail.com>
2025-07-04 14:12:59 +0700

    ref: connect to sql server

---
+++ Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GA20201.Models
+{
+    public class Item
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
