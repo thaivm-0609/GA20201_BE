b0cb72a23adc85c002bca402346d4731cc91b529
thaivm-0609 <vuongthai95@gmail.com>
2025-07-04 13:54:18 +0700

    feat: init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GA20201.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
