7947a2acf36602dc86e64b334fab9da079db6bd6
thaivm-0609 <vuongthai95@gmail.com>
2025-07-18 18:29:24 +0700

    feat: upload and swagger

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GA20201.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        //upload 1 file: UploadFile(<IFormFile> file)
+        //upload nhieu file: UploadFile(List<IFormFile> files)
+        //[FromForm]: khai bao du lieu se duoc gui qua form
+        public async Task<IActionResult> UploadFile([FromForm] List<IFormFile> files)
+        {
+            try
+            {
+                //B1: kiem tra xem ng dung co day file len khong?
+                if (files == null || files.Count == 0)
+                {
+                    return BadRequest("Ban chua chon file");
+                }
+                //B2: thuc hien upload file
+                //B2.1: khai bao vi tri luu tru file: path: la duong dan den thu muc chua file
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+                Directory.CreateDirectory(path); //tao thu muc neu nhu chua co
+
+                //B2.2: upload file len server
+                foreach (var file in files) 
+                {
+                    //duong dan den file = path + tenFile;
+                    var filePath = Path.Combine(path, file.FileName); 
+                    //su dung FileStream de tao luong upload file
+                    using FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await file.CopyToAsync(fs);
+                }
+
+                return Ok("Upload thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//khai bao AppDbContext de ket noi den SqlServer
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//khai bao Swagger: 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "GA20201", Version = "v1" });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +23,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//use Swagger va SwaggerUI
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint(
+    "/swagger/v1/swagger.json",
+    "GA20201"));
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
